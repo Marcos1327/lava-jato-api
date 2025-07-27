c325b2d426854a99e4b19a7c4af688039e052db6
Marcos Giovanny <giovannygigi@gmail.com>
2025-07-27 14:52:01 -0300

    criado os pacotes controllers, repositories, services

---
+++ src/main/java/com/lava_jato/repositories/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.lava_jato.repositories;
+
+import com.lava_jato.entities.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
---
+++ src/main/java/com/lava_jato/services/ClienteService.java
@@ -0,0 +1,8 @@
+package com.lava_jato.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+}
