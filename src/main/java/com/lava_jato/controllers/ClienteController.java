8c422319d481c1339dfd958b19aa5bc7feeb171e
Marcos Giovanny <giovannygigi@gmail.com>
2025-07-27 18:06:45 -0300

    inicio do metodo create pessoa fisica e funcionando

--- src/main/java/com/lava_jato/controllers/ClienteController.java
+++ src/main/java/com/lava_jato/controllers/ClienteController.java
@@ -1,10 +1,28 @@
 package com.lava_jato.controllers;
 
+import com.lava_jato.entities.dto.PessoaFisicaDTO;
+import com.lava_jato.entities.model.PessoaFisica;
+import com.lava_jato.repositories.ClienteRepository;
+import com.lava_jato.services.ClienteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/clientes")
 public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping("/create-pf")
+    public ResponseEntity<PessoaFisica> create(@RequestBody PessoaFisicaDTO pessoaFisicaDTO){
+        return ResponseEntity.ok(clienteService.create(pessoaFisicaDTO));
+    }
 
 }
--- src/main/java/com/lava_jato/services/ClienteService.java
+++ src/main/java/com/lava_jato/services/ClienteService.java
@@ -1,7 +1,11 @@
 package com.lava_jato.services;
 
+import com.lava_jato.entities.dto.PessoaFisicaDTO;
+import com.lava_jato.entities.model.PessoaFisica;
 import com.lava_jato.repositories.ClienteRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class ClienteService {
@@ -10,6 +14,18 @@ public class ClienteService {
 
     public ClienteService(ClienteRepository clienteRepository) {
         this.clienteRepository = clienteRepository;
+    }
+
+    public PessoaFisica create(PessoaFisicaDTO pessoaFisicaDTO) {
+        PessoaFisica pessoaFisica = new PessoaFisica();
+
+        pessoaFisica.setNome(pessoaFisicaDTO.getNome());
+        pessoaFisica.setTelefone(pessoaFisicaDTO.getTelefone());
+        pessoaFisica.setEmail(pessoaFisicaDTO.getEmail());
+        pessoaFisica.setObservacoes(pessoaFisicaDTO.getObservacoes());
+        pessoaFisica.setDataCriacao(LocalDate.now());
+
+        return clienteRepository.save(pessoaFisica);
     }
 
 }
